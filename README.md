ee919bcf00574dc2b895d0544b8b45db3f995b3f
Steve Day <staveday4u@gmail.com>
2019-07-07 18:05:49 +0200

    first in

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CopyRig
+
+Simple copy Rig to Auto Rig Pro reference rig. Applies position and scale of the known bones of an imported rig to a the auto rig pro reference rig.
+
+## Usage
+Apply pos, rot, scale to everything. ;-) Select the iported rig, go to edit mode, press copy. Select the reference rig, go to edit mode, press paste. 
+
+## Limitation
+- The code is probably horrible - it is my first blender addon and actually my first >10 lines python program.
+- The only supported import rig right now is the makehuman game engine rig.
---
+++ __init__.py
@@ -0,0 +1,158 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+bl_info = {
+    "name" : "CopyRig",
+    "author" : "Steve Day",
+    "description" : "",
+    "blender" : (2, 80, 0),
+    "version" : (0, 0, 1),
+    "location" : "",
+    "warning" : "",
+    "category" : "Generic"
+}
+
+import bpy, bmesh, mathutils, math, bpy_extras, ast, os, time
+
+class CPYRIG_PT_Panel(bpy.types.Panel):
+    bl_idname = "CPYRIG_PT_panel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_label = "Copy Rig"
+    # bl_context = "objectmode"
+    bl_category = "Copy Rig"
+
+    def draw(self, context):
+        col = self.layout.column()
+        col.label(text="Copy Rig")
+        col.operator('object.cpyrig_copy')
+        col.operator('object.cpyrig_paste')
+
+
+class CPYRIG_OT_copy(bpy.types.Operator):
+    bl_idname = "object.cpyrig_copy"
+    bl_label = "Copy"
+    bl_description = "Copy Rig Data"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object.type == 'ARMATURE' and context.active_object.mode == 'EDIT'
+
+    def execute(self, context):
+        CopyRig.copy()
+        return {'FINISHED'}
+    
+class CPYRIG_OT_paste(bpy.types.Operator):
+    bl_idname = "object.cpyrig_paste"
+    bl_label = "Paste"
+    bl_description = "Paste Rig Data"
+    bl_options = {'REGISTER', 'UNDO'}
+    
+    @classmethod
+    def poll(cls, context):
+        return CopyRig.has_data() and context.active_object.type == 'ARMATURE' and context.active_object.mode == 'EDIT'
+    
+    def execute(self, context):
+        CopyRig.paste()
+        return {'FINISHED'}
+
+bpy.utils.register_class(CPYRIG_OT_copy)
+bpy.utils.register_class(CPYRIG_OT_paste)
+bpy.utils.register_class(CPYRIG_PT_Panel)
+
+class CopyRig:
+    # makehuman game rig
+    translate = {
+    #'Root':'root_ref.x', 'pelvis': '', 'spine_01':'', 'spine_02':'', 'spine_03':'',
+    'clavicle_l':'shoulder_ref.l', 
+    'upperarm_l':'arm_ref.l', 
+    'lowerarm_l':'forearm_ref.l', 
+    'hand_l':'hand_ref.l', 
+    'index_01_l': 'index1_ref.l', 
+    'index_02_l': 'index2_ref.l', 
+    'index_03_l': 'index3_ref.l', 
+    'middle_01_l':'middle1_ref.l', 
+    'middle_02_l':'middle2_ref.l', 
+    'middle_03_l':'middle3_ref.l', 
+    'pinky_01_l':'pinky1_ref.l', 
+    'pinky_02_l':'pinky2_ref.l', 
+    'pinky_03_l':'pinky3_ref.l', 
+    'ring_01_l':'ring1_ref.l', 
+    'ring_02_l':'ring2_ref.l', 
+    'ring_03_l':'ring3_ref.l', 
+    'thumb_01_l':'thumb1_ref.l', 
+    'thumb_02_l':'thumb2_ref.l', 
+    'thumb_03_l':'thumb3_ref.l', 
+    'clavicle_r':'shoulder_ref.r',
+    'upperarm_r':'arm_ref.r', 
+    'lowerarm_r':'forearm_ref.r', 
+    'hand_r':'hand_ref.r', 
+    'index_01_r':'index1_ref.r', 
+    'index_02_r':'index2_ref.r',
+    'index_03_r':'index3_ref.r', 
+    'middle_01_r':'middle1_ref.r', 
+    'middle_02_r':'middle2_ref.r',
+    'middle_03_r':'middle3_ref.r',
+    'pinky_01_r':'pinky1_ref.r', 
+    'pinky_02_r':'pinky2_ref.r', 
+    'pinky_03_r':'pinky3_ref.r', 
+    'ring_01_r':'ring1_ref.r', 
+    'ring_02_r':'ring2_ref.r', 
+    'ring_03_r':'ring3_ref.r', 
+    'thumb_01_r':'thumb1_ref.r', 
+    'thumb_02_r':'thumb2_ref.r', 
+    'thumb_03_r':'thumb3_ref.r', 
+    'neck_01':'neck_ref.x', 
+    'head':'head_ref.x', 
+    'thigh_l':'thigh_ref.l', 
+    'calf_l':'leg_ref.l', 
+    'foot_l':'foot_ref.l', 
+    'ball_l':'toes_ref.l', 
+    'thigh_r':'thigh_ref.r', 
+    'calf_r':'leg_ref.r', 
+    'foot_r':'foot_ref.r', 
+    'ball_r':'toes_ref.r'
+    }
+    
+    __dict = {}
+   
+    @classmethod
+    def has_data(cls):
+        return bool(cls.__dict)
+
+    @classmethod
+    def copy(cls):
+        for bone in bpy.context.active_object.data.edit_bones:
+            if bone.name in cls.translate:
+                cls.__dict[cls.translate[bone.name]] = [bone.head[0], bone.head[1], bone.head[2]], [bone.tail[0], bone.tail[1], bone.tail[2]], bone.roll 
+
+    @classmethod
+    def paste(cls):
+        for bone in bpy.context.active_object.data.edit_bones:
+            if bone.layers[17] and bone.name in cls.__dict:
+                bone.head, bone.tail, bone.roll = cls.__dict[bone.name]
+
+
+classes = (
+)
+
+def register():
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
+
+def unregister():
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
